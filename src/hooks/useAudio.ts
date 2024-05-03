0e08a4a3134d32e7c63b23998776520430f687d2
justinzhang <justin.zhang@unition.global>
2024-05-03 18:01:05 +0800

    Added audio for sign

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -121,12 +121,91 @@ import s120 from '@/assets/sounds/sign/120.mp3'
 import s121 from '@/assets/sounds/sign/121.mp3'
 import s122 from '@/assets/sounds/sign/122.mp3'
 import s123 from '@/assets/sounds/sign/123.mp3'
+import s124 from '@/assets/sounds/sign/124.mp3'
+import s125 from '@/assets/sounds/sign/125.mp3'
+import s126 from '@/assets/sounds/sign/126.mp3'
+import s127 from '@/assets/sounds/sign/127.mp3'
+import s128 from '@/assets/sounds/sign/128.mp3'
+import s129 from '@/assets/sounds/sign/129.mp3'
+import s130 from '@/assets/sounds/sign/130.mp3'
+import s131 from '@/assets/sounds/sign/131.mp3'
+import s132 from '@/assets/sounds/sign/132.mp3'
+import s133 from '@/assets/sounds/sign/133.mp3'
+import s134 from '@/assets/sounds/sign/134.mp3'
+import s135 from '@/assets/sounds/sign/135.mp3'
+import s136 from '@/assets/sounds/sign/136.mp3'
+import s137 from '@/assets/sounds/sign/137.mp3'
+import s138 from '@/assets/sounds/sign/138.mp3'
+import s139 from '@/assets/sounds/sign/139.mp3'
+import s140 from '@/assets/sounds/sign/140.mp3'
+import s141 from '@/assets/sounds/sign/141.mp3'
+import s142 from '@/assets/sounds/sign/142.mp3'
+import s143 from '@/assets/sounds/sign/143.mp3'
+import s144 from '@/assets/sounds/sign/144.mp3'
+import s145 from '@/assets/sounds/sign/145.mp3'
+import s146 from '@/assets/sounds/sign/146.mp3'
+import s147 from '@/assets/sounds/sign/147.mp3'
+import s148 from '@/assets/sounds/sign/148.mp3'
+import s149 from '@/assets/sounds/sign/149.mp3'
+import s150 from '@/assets/sounds/sign/150.mp3'
+import s151 from '@/assets/sounds/sign/151.mp3'
+import s152 from '@/assets/sounds/sign/152.mp3'
+import s153 from '@/assets/sounds/sign/153.mp3'
+import s154 from '@/assets/sounds/sign/154.mp3'
+import s155 from '@/assets/sounds/sign/155.mp3'
+import s156 from '@/assets/sounds/sign/156.mp3'
+import s157 from '@/assets/sounds/sign/157.mp3'
+import s158 from '@/assets/sounds/sign/158.mp3'
+import s159 from '@/assets/sounds/sign/159.mp3'
+import s160 from '@/assets/sounds/sign/160.mp3'
+import s161 from '@/assets/sounds/sign/161.mp3'
+import s162 from '@/assets/sounds/sign/162.mp3'
+import s163 from '@/assets/sounds/sign/163.mp3'
+import s164 from '@/assets/sounds/sign/164.mp3'
+import s165 from '@/assets/sounds/sign/165.mp3'
+import s166 from '@/assets/sounds/sign/166.mp3'
+import s167 from '@/assets/sounds/sign/167.mp3'
+import s168 from '@/assets/sounds/sign/168.mp3'
+import s169 from '@/assets/sounds/sign/169.mp3'
+import s170 from '@/assets/sounds/sign/170.mp3'
+import s171 from '@/assets/sounds/sign/171.mp3'
+import s172 from '@/assets/sounds/sign/172.mp3'
+import s173 from '@/assets/sounds/sign/173.mp3'
+import s174 from '@/assets/sounds/sign/174.mp3'
+import s175 from '@/assets/sounds/sign/175.mp3'
+import s176 from '@/assets/sounds/sign/176.mp3'
+import s177 from '@/assets/sounds/sign/177.mp3'
+import s178 from '@/assets/sounds/sign/178.mp3'
+import s179 from '@/assets/sounds/sign/179.mp3'
+import s180 from '@/assets/sounds/sign/180.mp3'
+import s181 from '@/assets/sounds/sign/181.mp3'
+import s182 from '@/assets/sounds/sign/182.mp3'
+import s183 from '@/assets/sounds/sign/183.mp3'
+import s184 from '@/assets/sounds/sign/184.mp3'
+import s185 from '@/assets/sounds/sign/185.mp3'
+import s186 from '@/assets/sounds/sign/186.mp3'
+import s187 from '@/assets/sounds/sign/187.mp3'
+import s188 from '@/assets/sounds/sign/188.mp3'
+import s189 from '@/assets/sounds/sign/189.mp3'
+import s190 from '@/assets/sounds/sign/190.mp3'
+import s191 from '@/assets/sounds/sign/191.mp3'
+import s192 from '@/assets/sounds/sign/192.mp3'
+import s193 from '@/assets/sounds/sign/193.mp3'
+import s194 from '@/assets/sounds/sign/194.mp3'
+import s195 from '@/assets/sounds/sign/195.mp3'
+import s196 from '@/assets/sounds/sign/196.mp3'
+import s197 from '@/assets/sounds/sign/197.mp3'
+import s198 from '@/assets/sounds/sign/198.mp3'
+import s199 from '@/assets/sounds/sign/199.mp3'
+import s200 from '@/assets/sounds/sign/200.mp3'
+import s201 from '@/assets/sounds/sign/201.mp3'
+import s202 from '@/assets/sounds/sign/202.mp3'
 
 import {Ref, ref} from 'vue';
 import useData from '@/hooks/useData';
 
-const {signCounts,ruleCounts} = useData();
-const signAudioPath: string[] = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20, s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31, s32, s33, s34, s35, s36, s37, s38, s39, s40, s41, s42, s43, s44, s45, s46, s47, s48, s49, s50, s51, s52, s53, s54, s55, s56, s57, s58, s59, s60, s61, s62, s63, s64, s65, s66, s67, s68, s69, s70, s71, s72, s73, s74, s75, s76, s77, s78, s79, s80, s81, s82, s83, s84, s85, s86, s87, s88, s89, s90, s91, s92, s93, s94, s95, s96, s97, s98, s99, s100, s101, s102, s103, s104, s105, s106, s107, s108, s109, s110, s111, s112, s113, s114, s115, s116, s117, s118, s119, s120, s121, s122, s123]
+const {signCounts, ruleCounts} = useData();
+const signAudioPath: string[] = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20, s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31, s32, s33, s34, s35, s36, s37, s38, s39, s40, s41, s42, s43, s44, s45, s46, s47, s48, s49, s50, s51, s52, s53, s54, s55, s56, s57, s58, s59, s60, s61, s62, s63, s64, s65, s66, s67, s68, s69, s70, s71, s72, s73, s74, s75, s76, s77, s78, s79, s80, s81, s82, s83, s84, s85, s86, s87, s88, s89, s90, s91, s92, s93, s94, s95, s96, s97, s98, s99, s100, s101, s102, s103, s104, s105, s106, s107, s108, s109, s110, s111, s112, s113, s114, s115, s116, s117, s118, s119, s120, s121, s122, s123, s124, s125, s126, s127, s128, s129, s130, s131, s132, s133, s134, s135, s136, s137, s138, s139, s140, s141, s142, s143, s144, s145, s146, s147, s148, s149, s150, s151, s152, s153, s154, s155, s156, s157, s158, s159, s160, s161, s162, s163, s164, s165, s166, s167, s168, s169, s170, s171, s172, s173, s174, s175, s176, s177, s178, s179, s180, s181, s182, s183, s184, s185, s186, s187, s188, s189, s190, s191, s192, s193, s194, s195, s196, s197, s198, s199, s200, s201, s202]
 const ruleQuestionAudioPath: string[] = []
 const ruleAnswerAudioPath: string[] = []
 const isPlayingSignAudio: Ref<boolean[]> = ref([]);
@@ -159,15 +238,15 @@ const pauseAudio = () => {
     currentAudio.pause();
 }
 
-for(let i = 0; i < signCounts; i++) {
+for (let i = 0; i < signCounts; i++) {
     isPlayingSignAudio.value.push(false);
 }
 
-for(let i = 0; i < ruleCounts; i++) {
+for (let i = 0; i < ruleCounts; i++) {
     isPlayingRuleQuestionAudio.value.push(false);
 }
 
-for(let i = 0; i < ruleCounts; i++) {
+for (let i = 0; i < ruleCounts; i++) {
     isPlayingRuleAnswerAudio.value.push(false);
 }
 
