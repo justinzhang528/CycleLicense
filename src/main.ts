7e232de2c0c5df597da7578705bca724c9e8fe13
justinzhang <justin.zhang@unition.global>
2024-05-04 13:10:38 +0800

    Modified logic of increase life

--- src/main.ts
+++ src/main.ts
@@ -75,12 +75,16 @@ router.isReady().then(() => {
         setInterval(()=>{
             const currentTime = new Date().getTime();
             const latestIncreaseLifeTime = parseInt(localStorage.getItem('latestIncreaseLifeTime') || '0');
-            if(currentTime - latestIncreaseLifeTime >= UseData().DEFAULT_LIFE_INCREASE_INTERVAL){
-                const currentLife = parseInt(localStorage.getItem('currentLife') || '0');
-                if(currentLife < UseData().DEFAULT_LIFE){
-                    localStorage.setItem('currentLife', (currentLife + 1).toString());
+            const diff = currentTime - latestIncreaseLifeTime;
+            if(diff >= UseData().DEFAULT_LIFE_INCREASE_INTERVAL){
+                const increaseLifeCount = Math.trunc(diff / UseData().DEFAULT_LIFE_INCREASE_INTERVAL);
+                let currentLife = parseInt(localStorage.getItem('currentLife') || '0');
+                currentLife += increaseLifeCount;
+                if(currentLife > UseData().DEFAULT_LIFE){
+                    currentLife = UseData().DEFAULT_LIFE;
                 }
-                localStorage.setItem('latestIncreaseLifeTime', currentTime.toString());
+                localStorage.setItem('currentLife', currentLife.toString());
+                localStorage.setItem('latestIncreaseLifeTime', (currentTime+(diff%UseData().DEFAULT_LIFE_INCREASE_INTERVAL)).toString());
             }
         }, 60000);
     }
