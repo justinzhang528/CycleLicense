a7dce28a373e218aec3c4b9786369471106f6d2c
justinzhang <justin.zhang@unition.global>
2024-05-07 16:22:52 +0800

    Extract side menu from App.vue

---
+++ src/views/SideMenu.vue
@@ -0,0 +1,204 @@
+<template>
+  <IonMenu side="end" content-id="menu" menu-id="menu">
+    <IonHeader>
+      <IonToolbar>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonList>
+        <IonItem lines="none">
+          <IonGrid>
+            <IonRow class="ion-justify-content-center ion-padding">
+              <IonAvatar style="width: 75px; height: 75px">
+                <img v-if="userInfo.name" alt="avatar" :src="'images/avatar.png'">
+                <img v-if="!userInfo.name" alt="avatar" src="https://ionicframework.com/docs/img/demos/avatar.svg">
+              </IonAvatar>
+            </IonRow>
+            <IonRow class="ion-justify-content-center ion-padding">
+              <IonIcon v-if="userInfo.isUnlimited" color="warning" :icon="diamond"></IonIcon>
+              <IonLabel v-if="userInfo.name" style="padding: 0 5px 0 5px">{{userInfo.name}}</IonLabel>
+              <IonIcon v-if="userInfo.isUnlimited" color="warning" :icon="diamond"></IonIcon>
+              <span @click="openLoginModal">
+                  <IonButton v-if="!userInfo.name" color="dark" shape="round">{{$t('login')}}</IonButton>
+                </span>
+            </IonRow>
+            <IonRow class="ion-justify-content-center">
+                <span v-for="i in life.totalLife" v-if="userInfo.name && !userInfo.isUnlimited">
+                  <IonIcon v-if="life.currentLife>=i" :icon="heart" style="font-size: 22px" color="warning"></IonIcon>
+                  <IonIcon v-if="life.currentLife<i" :icon="heartOutline" style="font-size: 22px" color="warning"></IonIcon>
+                </span>
+              <span class="center" v-if="userInfo.isUnlimited">
+                  <img :src="'images/icon/unlimitedIcon.png'" alt="unlimited" style="width: 15%"/>
+                </span>
+            </IonRow>
+            <IonRow class="ion-justify-content-center ion-padding" v-if="userInfo.name" >
+              <IonButton fill="clear" color="dark" shape="round" style="text-decoration: underline" @click="onLogoutClick">
+                <IonIcon color="dark" :icon="logOut" style="padding-right: 5px"></IonIcon>
+                {{$t('logout')}}
+              </IonButton>
+            </IonRow>
+            <IonRow class="ion-justify-content-center ion-padding-bottom" >
+                <span @click="openRegisterModal">
+                  <IonButton v-if="!userInfo.name" fill="clear" color="dark" shape="round" style="text-decoration: underline">
+                    <IonIcon color="dark" :icon="personAdd" style="padding-right: 5px"></IonIcon>
+                    {{$t('register')}}
+                  </IonButton>
+                </span>
+            </IonRow>
+          </IonGrid>
+        </IonItem>
+        <IonItem style="padding-top: 10px; padding-bottom: 10px">
+          <IonThumbnail slot="start">
+            <img alt="language" :src="'images/icon/languageIcon.png'">
+          </IonThumbnail>
+          <IonLabel>{{ $t('language') }}</IonLabel>
+          <IonSelect style="font-weight: bold" aria-label="Language" interface="popover" :value="currentSelectedLanguageValue" @ionChange="onSelectedLanguageChange">
+            <IonSelectOption value="en">English</IonSelectOption>
+            <IonSelectOption value="mm">မြန်မာ</IonSelectOption>
+            <IonSelectOption value="zh_cn">简体中文</IonSelectOption>
+            <IonSelectOption value="zh_tw">繁體中文</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        <IonItem style="padding-top: 10px; padding-bottom: 10px" class="center">
+          <IonThumbnail slot="start">
+            <img alt="notice" :src="'images/icon/taiwanMyanmarFlagIcon.png'">
+          </IonThumbnail>
+          <span style="width: 100%;">
+              <IonButton :disabled="!userInfo.name" @click="openAboutTaiwanModal" size="default" fill="clear" color="dark" style="text-decoration: underline;">
+                {{ $t('aboutTaiwan') }}
+              </IonButton>
+            </span>
+        </IonItem>
+        <IonItem class="center">
+          <IonThumbnail slot="start">
+            <img alt="donate" :src="'images/icon/donateIcon.png'">
+          </IonThumbnail>
+          <span style="width: 100%; padding-bottom: 10px">
+              <IonButton :disabled="!userInfo.name" @click="openDonateModal" size="default" fill="clear" color="dark" style="text-decoration: underline;">
+                {{ $t('donate') }}
+              </IonButton><br>
+              <IonLabel color="medium">({{$t('unlockUnlimitedFeature')}})</IonLabel>
+            </span>
+        </IonItem>
+      </IonList>
+
+    </IonContent>
+  </IonMenu>
+</template>
+
+<script setup lang="ts">
+import {
+  IonContent,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonSelectOption,
+  IonSelect,
+  IonLabel,
+  IonList,
+  IonItem,
+  IonThumbnail,
+  IonButton,
+  IonIcon,
+  IonAvatar,
+  IonRow,
+  IonGrid,
+  modalController,
+} from '@ionic/vue';
+import {onMounted, ref} from "vue";
+import {useI18n} from "vue-i18n";
+import {diamond, heart, heartOutline, logOut, personAdd} from "ionicons/icons";
+import {showAlertWithAction} from "@/hooks/useUtils";
+import useData from "@/hooks/useData";
+import scheduleNotification from "@/hooks/useLocalNotification";
+import LoginModal from "@/views/Modal/LoginModal.vue";
+import RegisterModal from "@/views/Modal/RegisterModal.vue";
+import AboutTaiwanModal from "@/views/Modal/AboutTaiwanModal.vue";
+import DonateModal from "@/views/Modal/DonateModal.vue";
+
+const {t,locale} = useI18n();
+const currentSelectedLanguageValue = ref(localStorage.getItem('currentLanguage') || 'en');
+let userInfo = ref(JSON.parse(localStorage.getItem('userInfo') || '{}'));
+const {life} = useData();
+
+const onSelectedLanguageChange = (e: CustomEvent)=>{
+  currentSelectedLanguageValue.value = e.detail.value;
+  if(currentSelectedLanguageValue.value == 'en')
+    locale.value = 'en'
+  else if(currentSelectedLanguageValue.value == 'mm')
+    locale.value = 'mm';
+  else if(currentSelectedLanguageValue.value == 'zh_cn')
+    locale.value = 'zh_cn';
+  else if(currentSelectedLanguageValue.value == 'zh_tw')
+    locale.value = 'zh_tw';
+  localStorage.setItem('currentLanguage',currentSelectedLanguageValue.value);
+  scheduleNotification(t('drivingLicense'),t('timeToStudy'));
+}
+
+const onLogoutClick = ()=>{
+  showAlertWithAction(t('warning'), '', t('areYouSureToLogout'), t('confirm'), t("cancel"), ()=>{
+    localStorage.removeItem('userInfo');
+    userInfo.value = {};
+    const nav = document.querySelector('ion-nav');
+    nav?.popToRoot();
+  });
+}
+
+const openLoginModal = async () => {
+  const modal = await modalController.create({
+    component: LoginModal,
+  });
+
+  modal.present();
+
+  const { data, role } = await modal.onWillDismiss();
+
+  if (role === 'confirm') {
+    userInfo.value = data;
+  }
+};
+
+const openRegisterModal = async () => {
+  const modal = await modalController.create({
+    component: RegisterModal,
+  });
+
+  modal.present();
+
+  const { data, role } = await modal.onWillDismiss();
+
+  if (role === 'confirm') {
+    userInfo.value = data;
+  }
+};
+
+const openAboutTaiwanModal = async () => {
+  const modal = await modalController.create({
+    component: AboutTaiwanModal,
+  });
+
+  modal.present();
+};
+
+const openDonateModal = async () => {
+  const modal = await modalController.create({
+    component: DonateModal,
+  });
+
+  modal.present();
+};
+
+onMounted(()=> {
+  setInterval(()=>{
+    if(localStorage.getItem('currentLife')){
+      life.value.currentLife = Number(localStorage.getItem('currentLife'));
+    }
+  }, 3000);
+});
+</script>
+
+<style>
+ion-thumbnail {
+  --size: 40px;
+}
+</style>
