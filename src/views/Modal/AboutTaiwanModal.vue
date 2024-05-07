2d69c4b8f2c6bd56a35aa548bcf7fb3e188ffd70
justinzhang <justin.zhang@unition.global>
2024-05-07 19:05:07 +0800

    Add test rules page

--- src/views/Modal/AboutTaiwanModal.vue
+++ src/views/Modal/AboutTaiwanModal.vue
@@ -9,17 +9,39 @@
   </IonHeader>
   <IonContent class="center ion-padding">
     <img alt="notice" :src="'images/noticeInformation.png'" style="width: 80%">
-    <IonItem>
-
+    <IonItem button detail lines="none" class="ion-item-border" @click="onClickTestNotice">
+      <IonThumbnail slot="start">
+        <img alt="language" :src="'images/icon/testRulesIcon.png'">
+      </IonThumbnail>
+      <IonLabel style="padding-left: 10px">{{$t('testRules')}}</IonLabel>
     </IonItem>
   </IonContent>
 </template>
 
 <script setup lang="ts">
-import {IonButton, IonButtons, IonContent, IonHeader, IonItem, IonTitle, IonToolbar, modalController} from "@ionic/vue";
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonItem, IonLabel,
+  IonThumbnail,
+  IonTitle,
+  IonToolbar,
+  modalController,
+} from "@ionic/vue";
+import TestNoticeModal from "@/views/Modal/TestRulesModal.vue";
 
 const onCancelAboutTaiwanModal = () => {
   modalController.dismiss(null, 'cancel');
+}
+
+const onClickTestNotice = async () => {
+  const modal = await modalController.create({
+    component: TestNoticeModal,
+  });
+
+  modal.present();
 }
 </script>
 
---
+++ src/views/Modal/TestRulesModal.vue
@@ -0,0 +1,79 @@
+<template>
+  <IonHeader>
+    <IonToolbar>
+      <IonButtons slot="start">
+        <IonButton @click="onCancelDonateModal">{{ $t('cancel') }}</IonButton>
+      </IonButtons>
+      <IonTitle class="center">{{ $t('testRules') }}</IonTitle>
+    </IonToolbar>
+  </IonHeader>
+  <IonContent class="center ion-padding">
+    <IonGrid class="ion-text-left">
+      <IonRow>
+        <IonLabel>{{$t('notification')}}</IonLabel>
+      </IonRow>
+      <IonRow>
+        <IonCol size="1">
+          <IonLabel>{{$t('1')}}{{$t('.')}}</IonLabel>
+        </IonCol>
+        <IonCol size="11">
+          <IonLabel>{{$t('testRules-1')}}</IonLabel>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol size="1">
+          <IonLabel>{{$t('2')}}{{$t('.')}}</IonLabel>
+        </IonCol>
+        <IonCol size="11">
+          <IonLabel>{{$t('testRules-2')}}</IonLabel>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol size="1">
+          <IonLabel>{{$t('3')}}{{$t('.')}}</IonLabel>
+        </IonCol>
+        <IonCol size="11">
+          <IonLabel>{{$t('testRules-3')}}</IonLabel>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol size="1">
+          <IonLabel>{{$t('4')}}{{$t('.')}}</IonLabel>
+        </IonCol>
+        <IonCol size="11">
+          <IonLabel>{{$t('testRules-4')}}</IonLabel>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol size="1">
+          <IonLabel>{{$t('5')}}{{$t('.')}}</IonLabel>
+        </IonCol>
+        <IonCol size="11">
+          <IonLabel>{{$t('testRules-5')}}</IonLabel>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol size="1">
+          <IonLabel>{{$t('6')}}{{$t('.')}}</IonLabel>
+        </IonCol>
+        <IonCol size="11">
+          <IonLabel>{{$t('testRules-6')}}</IonLabel>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  </IonContent>
+</template>
+
+<script setup lang="ts">
+import {IonButton, IonButtons, IonContent, IonHeader, IonItem, IonTitle, IonToolbar, modalController, IonGrid, IonRow, IonCol, IonLabel} from "@ionic/vue"
+
+const onCancelDonateModal = () => {
+  modalController.dismiss(null, 'cancel')
+}
+</script>
+
+<style scoped>
+ion-grid {
+  --ion-grid-columns: 12;
+}
+</style>
